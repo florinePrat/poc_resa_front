2f1135c580b2cd895167c8e98a4cdd2d252bd077
florinePrat <florineptlg64@gmail.com>
2022-09-07 20:12:03 +0200

    init front repo

--- resa_front/src/views/AboutView.vue
+++ resa_front/src/views/AboutView.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <form>
+
+<div class="uk-margin">
+    <div class="uk-inline">
+        <span class="uk-form-icon" uk-icon="icon: user"></span>
+        <input class="uk-input" type="text">
+    </div>
+</div>
+
+<div class="uk-margin">
+    <div class="uk-inline">
+        <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
+        <input class="uk-input" type="text">
+    </div>
+</div>
+
+</form>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
